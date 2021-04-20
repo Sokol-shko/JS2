d037b9a14f95705db4d4b91d3aa769fae7c22170
Sokol-shko <sokol-shkoldin@mail.ru>
2021-04-20 11:15:11 +0400

    Добавил структуру Задания 3

---
+++ Shkoldin_HW2(Задание 3)/script.js
@@ -0,0 +1,19 @@
+// class Hamburger {
+//     constructor(size, stuffing) {
+//
+//     }
+// }
+//
+//     addTopping(topping) {}    // Добавить добавку }
+//     removeTopping(topping) { // Убрать добавку }
+//     getToppings(topping) {   // Получить список добавок }
+//     getSize() {              // Узнать размер гамбургера }
+//     getStuffing() {          // Узнать начинку гамбургера }
+//     calculatePrice() {       // Узнать цену }
+//     calculateCalories() {    // Узнать калорийность }
+// }
+
+гамбургер
+конструктор (размер, начинка)
+// размеры: маленький, большой
+// начинки:
