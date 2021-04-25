87354deff0e3043a72d60afe28c763b0c52f25af
Sokol-shko <sokol-shkoldin@mail.ru>
2021-04-25 22:08:39 +0400

    Сделал ДЗ-3. Подключил adapter.js

--- script.js
+++ script.js
@@ -1,74 +1,38 @@
-// ************************************* Задание 1. Классы корзины и элемента корзины
-class ProductsCart {
-  sum = 0;
+'use strict';
 
-  calculateSum (a, b) {
-    this.sum = a + b;
-    return this.sum;
-  }
+import makeGETRequest from './adapter.js';
 
-  buy () {
-
-  }
-}
-
-class ProductsCartElement {
-  // цена товара
-  priceProduct = 0;
-
-  // кол-во товара
-  count = 0;
-
-  // изменить кол-во товара
-  changeCount () {
-  }
-
-  // убрать из корзины
-  remove () {
-
-  }
-}
-
+//**********************
 class GoodsItem {
   constructor (title = 'title', price = 0) {
-    this.title = title;
+    this.product_name = title;
     this.price = price;
   }
+
   render () {
-    return `
-      <div class="goods-item">
-        <h3>${this.title}</h3>
-        <p>${this.price}</p>
-      </div>
-     `;
+    return `<div class="goods-item"><h3>${this.product_name}</h3><p>${this.price}</p></div>`;
   }
 }
 
+//**********************
 class GoodsList {
   sumGoods = 0;
   constructor () {
     this.goods = [];
   }
   fetchGoods () {
-    this.goods = [
-      { title: 'Shirt', price: 150 },
-      { title: 'Socks', price: 50 },
-      { title: 'Jacket', price: 350 },
-      { title: 'Shoes', price: 250 },
-      { title: 'Shirt', price: 150 },
-      { title: 'Socks', price: 50 },
-      { title: 'Jacket', price: 350 },
-      { title: 'Shoes', price: 250 },
-      { title: 'Shirt', price: 150 },
-      { title: 'Socks', price: 50 },
-      { title: 'Jacket', price: 350 },
-      { title: 'Shoes', price: 250 }
-    ];
+    return makeGETRequest(`/catalogData.json`)
+        .then((goods) => {
+          this.goods = JSON.parse(goods);
+        })
   }
+
+
+
   render () {
     let listHtml = '';
     this.goods.forEach(good => {
-      const goodItem = new GoodsItem(good.title, good.price);
+      const goodItem = new GoodsItem(good.product_name, good.price);
       listHtml += goodItem.render();
     });
     document.querySelector('.goods-list').innerHTML = listHtml;
@@ -81,14 +45,48 @@ class GoodsList {
   }
 }
 
-// Вызываем методы
+//**********************
+class ProductsCart {
+  constructor () {
+    this.sum = 0;
+  }
+
+  calculateSum (a, b) {
+    this.sum = a + b;
+    return this.sum;
+  }
+
+  buy () {
+
+  }
+}
+
+//**********************
+class ProductsCartElement {
+  constructor () {
+    // цена товара
+    this.priceProduct = 0;
+    // кол-во товара
+    this.count = 0;
+  }
+
+  // изменить кол-во товара
+  changeCount () {}
+  // добавить в корзину
+  addToCart () {}
+  // убрать из корзины
+  removeFromCart () {}
+  // получение списка товаров корзины
+  getListProductsCart () {}
+}
+
+
+//**********************
 
 const  list = new GoodsList();
-list.fetchGoods();
-list.render();
+list.fetchGoods()
+    .then(() => list.render());
 
-
-// ************************************* Задание 2. Сумма всех товаров
 list.calculateSumAllGoods();
-alert(`${list.sumGoods} руб`);
+//alert(`${list.sumGoods} руб`);
 
