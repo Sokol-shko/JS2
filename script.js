401cfbf2829e3d511647427bd439d02dd0c6b923
Sokol-shko <sokol-shkoldin@mail.ru>
2021-04-30 08:34:50 +0400

    Будет развёрнут Vue.js (код не работает, разбираюсь)

--- script.js
+++ script.js
@@ -1,94 +1,107 @@
-// ********************** Задание 1-2
+'use strict';
 
-let inputText = '';
-let resultText = '';
+import makeGETRequest from './adapter.js';
 
-let regexp = /^(\')|(\')$|(\B\')|(\'\B)|(\'\')/g;
+//**********************
+class GoodsItem {
+  constructor (title = 'title', price = 0) {
+    this.product_name = title;
+    this.price = price;
+  }
 
-let res = document.querySelector('#resultText');
-
-let change = document.querySelector('#change');
-change.addEventListener('click', () => {
-    inputText = document.querySelector('#inputText').value;
-    console.log(regexp.test(inputText));
-    resultText = inputText.replace(regexp, '"');
-    res.innerHTML = resultText;
-});
-
-// ********************** Задание 3
-
-function escape(string) {
-    let htmlEscapes = {
-        '&': '&amp;',
-        '<': '&lt;',
-        '>': '&gt;',
-        '"': '&quot;',
-        "'": '&#39;'
-    };
-
-    return string.replace(/[&<>"']/g, function(match) {
-        return htmlEscapes[match];
-    });
+  render () {
+    return `<div class="goods-item"><h3>${this.product_name}</h3><p>${this.price}</p></div>`;
+  }
 }
 
-class FormValidation {
-    constructor () {
-        this.pattern = '';
-        this.name = '';
-        this.phone = '';
-        this.mail = '';
-        this.text = '';
-    }
-
-    checkValidation () {
-        let sendButton = document.querySelector('#send');
-        sendButton.addEventListener('click', (e) => {
-            // проверка имени
-            this.pattern = /^([a-zа-яё]+)$/ig;
-            let inputName = document.querySelector('#name');
-            this.name = inputName.value;
-            let errorName = document.querySelector('#error_name');
-
-            if (this.pattern.test(this.name.trim())) {
-                errorName.innerHTML = '';
-                inputName.classList.remove('error_border');
-            } else {
-                errorName.innerHTML = 'Ошибка! Проверьте требование имени';
-                inputName.classList.add('error_border');
-            }
-
-            // проверка телефона
-            this.pattern = /^\+7\(\d{3}\)\d{3}-\d{4}$/ig;
-            let inputPhone = document.querySelector('#phone');
-            this.phone = inputPhone.value;
-            let errorPhone = document.querySelector('#error_phone');
-            if (this.pattern.test(this.phone.trim())) {
-                errorPhone.innerHTML = '';
-                inputPhone.classList.remove('error_border');
-            } else {
-                errorPhone.innerHTML = 'Ошибка! Проверьте требование номера телефона';
-                inputPhone.classList.add('error_border');
-            }
-
-            // проверка E-mail
-            this.pattern = /^[a-z\d]+(\.?[_a-z\d-]+)*@[a-z\d-]+(\.[a-z\d-]+)*(\.[a-z]{2,})$/ig;
-            let inputMail = document.querySelector('#mail');
-            this.mail = inputMail.value;
-            let errorMail = document.querySelector('#error_mail');
-            if (this.pattern.test(this.mail.trim())) {
-                errorMail.innerHTML = '';
-                inputMail.classList.remove('error_border');
-            } else {
-                errorMail.innerHTML = 'Ошибка! Проверьте требование E-mail';
-                inputMail.classList.add('error_border');
-            }
-
-            // текст по условию любой
-            this.text = document.querySelector('#text').value;
-            console.log(this.text = escape(this.text));
+//**********************
+class GoodsList {
+  constructor () {
+    this.sumGoods = 0;
+    this.goods = [];
+    this.filteredGoods = [];
+  }
+  fetchGoods () {
+    return makeGETRequest(`/catalogData.json`)
+        .then((goods) => {
+          this.goods = JSON.parse(goods);
+          this.filteredGoods = JSON.parse(goods);
         })
-    }
+  }
+
+  filterGoods(value) {
+    const regexp = new RegExp(value, 'i');
+    this.filteredGoods = this.goods.filter(good =>
+        regexp.test(good.product_name));
+    this.render();
+  }
+
+  render () {
+    let listHtml = '';
+    this.filteredGoods.forEach(good => {
+      const goodItem = new GoodsItem(good.product_name, good.price);
+      listHtml += goodItem.render();
+    });
+    document.querySelector('.goods-list').innerHTML = listHtml;
+  }
+
+  calculateSumAllGoods () {
+    this.goods.forEach(good => {
+      this.sumGoods += good.price;
+    });
+    return this.sumGoods;
+  }
 }
 
-let fv = new FormValidation();
-fv.checkValidation();
+//**********************
+class ProductsCart {
+  constructor () {
+    this.sum = 0;
+  }
+
+  calculateSum (a, b) {
+    this.sum = a + b;
+    return this.sum;
+  }
+
+  buy () {
+
+  }
+}
+
+//**********************
+class ProductsCartElement {
+  constructor () {
+    // цена товара
+    this.priceProduct = 0;
+    // кол-во товара
+    this.count = 0;
+  }
+
+  // изменить кол-во товара
+  changeCount () {}
+  // добавить в корзину
+  addToCart () {}
+  // убрать из корзины
+  removeFromCart () {}
+  // получение списка товаров корзины
+  getListProductsCart () {}
+}
+
+
+//**********************
+
+const  list = new GoodsList();
+list.fetchGoods()
+    .then(() => list.render());
+
+list.calculateSumAllGoods();
+//alert(`${list.sumGoods} руб`);
+
+const searchInput = document.querySelector('.goods-search');
+
+const searchButton = document.querySelector('.search-button');
+searchButton.addEventListener('click', (e) => {
+  const value = searchInput.value;
+  list.filterGoods(value);
+});
