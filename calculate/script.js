e7f0ff6d762dda4c7806f3c0810d15986c705d1c
Sokol-shko <sokol-shkoldin@mail.ru>
2021-05-17 20:13:33 +0400

    Накидал 3 задание

--- calculate/script.js
+++ calculate/script.js
@@ -1,16 +1,24 @@
-const app = new Vue({
-    el: '#app',
-    data: {
-        a: '',
-        b: ''
-    },
-    methods: {
-        calc (a, b) {
-           return console.log(a + b);
-        }
-    }
-});
+let arg_one = document.querySelector('#arg-one');
+
+const addition = (a, b) => {
+    return a + b;
+};
+
+const subtraction = (a, b) => {
+    return a - b;
+};
+
+const multiplication = (a, b) => {
+    return a * b;
+};
+
+const division = (a, b) => {
+    return a / b;
+};
 
 module.exports = {
-    calc: app.calc()
+    addition: addition,
+    subtraction: subtraction,
+    multiplication: multiplication,
+    division: division
 };
--- calculate/spec/script.spec.js
+++ calculate/spec/script.spec.js
@@ -1,8 +1,32 @@
 const script = require('../script.js');
-const calc = script.calc;
+const addition = script.addition;
+const subtraction = script.subtraction;
+const multiplication = script.multiplication;
+const division = script.division;
 
-describe('Функция сложения calc()', () => {
+describe('Функция сложения addition()', () => {
     it('должна возвращать 12 при аргументах (10, 2)', () => {
-        expect(calc(10, 2)).toBe(12);
+        expect(addition(10, 2)).toBe(12);
+    })
+});
+
+describe('Функция вычитания subtraction()', () => {
+    it('должна возвращать 8 при аргументах (10, 2)', () => {
+        expect(subtraction(10, 2)).toBe(8);
+    });
+    it('должна возвращать -4 при аргументах (6, 10)', () => {
+        expect(subtraction(6, 10)).toBe(-4);
+    })
+});
+
+describe('Функция умножения multiplication()', () => {
+    it('должна возвращать 20 при аргументах (10, 2)', () => {
+        expect(multiplication(10, 2)).toBe(20);
+    })
+});
+
+describe('Функция деления division()', () => {
+    it('должна возвращать 5 при аргументах (10, 2)', () => {
+        expect(division(10, 2)).toBe(5);
     })
 });
